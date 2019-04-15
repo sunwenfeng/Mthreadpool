614514c4ab97ed91f38eb15d2f223a01cc575e5c
sunwenfeng <2655696423@qq.com>
2019-04-15 15:59:44 +0800

    first commit

---
+++ threadpool_01/cmake-build-debug/CMakeFiles/threadpool_01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/threadpool_01.dir/main.cpp.o"
+  "threadpool_01.pdb"
+  "threadpool_01"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/threadpool_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
